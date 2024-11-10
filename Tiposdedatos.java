bb534e3e6b29698eb66dd459df2db21e01d4ea17
EstebanMr003 <esmorales5@espe.edu.ec>
2024-11-09 22:05:28 -0500

    Add files via upload

---
+++ Tiposdedatos.java
@@ -0,0 +1,24 @@
+
+package main;
+public class Tiposdedatos {
+    
+    public static void main(String[] args){
+        //Tipos de datos primitivos
+        byte tipoByte = 100;
+        short tipoShort = 32000;
+        int tipoInt = 200000;
+        long tipoLong = 100000L;
+        float tipoFloat = 10.5f;
+        double tipoDouble = 20.99;
+        char tipoChar = 'A';
+        boolean tipoBoolean = true;
+        
+        //Tipos de datos de referencia
+         String tipoString = "Hola, Alumno!";
+        int[] tipoArray = {1,2,3,4,5};
+        
+         System.out.println("Tipo byte: " + tipoByte);
+        System.out.println("Tipo short: " + tipoShort);
+    }
+   }
+    
